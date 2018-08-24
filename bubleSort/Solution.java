1632e776a24a02984ecd0865e5efb15edb387d2b
Viniciuswps <vinicius.barbosa.silva@ccc.ufcg.edu.br>
2018-08-23 23:37:38 -0300

    corrigindo o bubleSort

--- bubleSort/Solution.java
+++ bubleSort/Solution.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Solution {
+class Solution {
 
 	public static void main(String[] args) {
 
@@ -10,41 +10,60 @@ public class Solution {
 		for (int i = 0; i < entrada.length; i++) {
 			array[i] = Integer.parseInt(entrada[i]);
 		}
-		
+
 		bubleSort(array);
-		
-		
+
 		scan.close();
 
 	}
 
 	private static void bubleSort(int[] array) {
-		boolean flag = false;
-		boolean ordenado = false;
-		while (!ordenado) {
-			
-			flag = false;
-			for (int i = 1; i < array.length; i++) {
-				if (array[i] < array[i - 1]) {
-					int aux = array[i - 1];
-					array[i - 1] = array[i];
-					array[i] = aux;
-					flag = true;
-				}
-			}
 
-			if (!flag) {
-				ordenado = true;
-			}
+		boolean flag = false;
+		boolean ordenado = arrayOrdenado(array);
+
+		if (ordenado) {
 			System.out.println(imprimeArray(array));
+		} 
+		else {
+			while (!ordenado) {
+				flag = false;
+				for (int i = 1; i < array.length; i++) {
+					if (array[i] < array[i - 1]) {
+						int aux = array[i - 1];
+						array[i - 1] = array[i];
+						array[i] = aux;
+						flag = true;
+					}
+				}
+				if (!flag) {
+					ordenado = true;
+				} else {
+					System.out.println(imprimeArray(array));
+				}
+
+			}
 		}
+	}
+
+	private static boolean arrayOrdenado(int[] array) {
+		for (int i = 1; i < array.length; i++) {
+			if (array[i] < array[i - 1]) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 	private static String imprimeArray(int[] array) {
 		String resultado = "";
-		for (int i = 0; i < array.length; i++) {
+
+		int i;
+		for (i = 0; i < array.length - 1; i++) {
 			resultado += array[i] + " ";
 		}
+		resultado += array[array.length - 1] + "";
+
 		return resultado;
 	}
 
