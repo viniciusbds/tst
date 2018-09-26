83617f566a15459f66ef0247413f0d9f9402feb5
Viniciuswps <vinicius.barbosa.silva@ccc.ufcg.edu.br>
2018-09-26 10:35:48 -0300

    add FilaComArray

--- FilaComArray/FilaComArray.java
+++ FilaComArray/FilaComArray.java
@@ -1,33 +1,46 @@
 import java.util.Scanner;
 
-public class FilaComArray {
+class FilaComArray {
 
 	private int[] array;
 	private int tail;
 
 	public FilaComArray(int tamanho) {
 		this.array = new int[tamanho];
-		this.tail = 0;
+		this.tail = -1;
 	}
 
-	public void enqueue(int numero) {
+	public void enqueue(int elemento) {
 		if (!isFull()) {
-			array[++tail] = numero;
+			this.array[++tail] = elemento;
+		} else {
+			System.out.println("full");
 		}
 	}
 
 	public int dequeue() {
 		int result = -1;
 		if (!isEmpty()) {
-			result = array[tail--];
+			result = array[0];
+			shiftLeft();
+			this.tail--;
+		} else {
+			System.out.println("empty");
 		}
 		return result;
 	}
 
-	public int head() {
-		int result = -1;
+	private void shiftLeft() {
+		for (int i = 0; i < tail; i++) {
+			array[i] = array[i + 1];
+		}
+
+	}
+
+	public String head() {
+		String result = "empty";
 		if (!isEmpty()) {
-			result = array[tail];
+			result = "" + array[tail];
 		}
 		return result;
 	}
@@ -41,41 +54,39 @@ public class FilaComArray {
 	}
 
 	public String print() {
-		return "";
+		String result = "empty";
+		if (!isEmpty()) {
+			result = "";
+			for (int i = 0; i < tail; i++) {
+				result += array[i] + " ";
+			}
+			result += array[tail] + "";
+		}
+		return result;
 	}
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+
 		int tamanho = Integer.parseInt(scan.nextLine());
 		FilaComArray fila = new FilaComArray(tamanho);
 
-		/*
-		 * 
-		 * Seu programa deve imprimir o conteúdo da fila sempre que uma operação print
-		 * for lida. Se a fila estiver vazia, imprima "empty". Se estiver cheia, imprima
-		 * "full".
-		 * 
-		 * Seu programa deve imprimir o elemento inicial da fila quando ler a operação
-		 * element.
-		 * 
-		 * Se a fila estiver vazia e for lida a operação remove, imprima "empty".
-		 * 
-		 * Se a fila estiver cheia e for lida a operação add, imprima "full".
-		 */
+		String[] entrada;
 		String operacao = "";
 		while (!operacao.equals("end")) {
 
-			operacao = scan.nextLine();
+			entrada = scan.nextLine().split(" ");
+			operacao = entrada[0];
+
 			if (operacao.equals("add")) {
-
+				int elemento = Integer.parseInt(entrada[1]);
+				fila.enqueue(elemento);
 			} else if (operacao.equals("remove")) {
-
+				fila.dequeue();
 			} else if (operacao.equals("element")) {
-
+				System.out.println(fila.head());
 			} else if (operacao.equals("print")) {
-
-			} else {
-
+				System.out.println(fila.print());
 			}
 
 		}
