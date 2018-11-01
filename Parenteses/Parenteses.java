ad59f84af81acadc2574d1d1fd1c6a69b55c7704
viniciusbds <vinicius.barbosa.silva@ccc.ufcg.edu.br>
2018-11-01 11:29:55 -0300

    refatorando Parenteses : usando a melhor estrutura de dados para este problema

--- Parenteses/Parenteses.java
+++ Parenteses/Parenteses.java
@@ -1,5 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 class Parenteses {
@@ -8,35 +7,36 @@ class Parenteses {
 		Scanner scan = new Scanner(System.in);
 		String[] entrada = scan.nextLine().split("");
 
-		List<String> sequencia = new ArrayList<String>();
+		LinkedList<String> sequenciaList = new LinkedList<String>();
 		for (String elemento : entrada) {
-			sequencia.add(elemento);
+			sequenciaList.add(elemento);
 		}
-		
-		if (sequenciaValida(sequencia)) {
+
+		if (sequenciaValida(sequenciaList)) {
 			System.out.println("S");
 		} else {
 			System.out.println("N");
 		}
-		
+
 		scan.close();
 	}
 
-	private static boolean sequenciaValida(List<String> sequencia) {
-		
-		if (sequencia.size() % 2 != 0) return false;
-		
+	private static boolean sequenciaValida(LinkedList<String> sequencia) {
+
+		if (sequencia.size() % 2 != 0)
+			return false;
+
 		boolean result = true;
 
 		String primeiro, ultimo;
 		while (result && sequencia.size() > 0) {
-			primeiro = sequencia.get(0);
-			ultimo = sequencia.get(sequencia.size() - 1);
+			primeiro = sequencia.peekFirst();
+			ultimo = sequencia.peekLast();
 			if (!primeiro.equals("(") || primeiro.equals(ultimo)) {
 				result = false;
 			}
-			sequencia.remove(0);
-			sequencia.remove(sequencia.size() - 1);
+			sequencia.removeFirst();
+			sequencia.removeLast();
 		}
 
 		return result;
